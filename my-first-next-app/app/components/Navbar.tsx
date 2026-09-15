b629dcf54bb0eee5cc8e7c9d4ee769a373c7e0cd
MahrooshNaeemAnwar <mahrooshnaeem10@gmail.com>
2026-09-15 20:02:48 +0500

    Add Navbar and Footer components

--- my-first-next-app/app/components/Navbar.tsx
+++ my-first-next-app/app/components/Navbar.tsx
@@ -1,18 +1,26 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+
 export default function Navbar(){
-    return(
-        <nav className="bg-gray-800 text-white p-4">
-            <div className="max-w-6xl mx-auto flex justify-between items-center">
-                {/*Logo*/}
-                <Link href="/" className="text-xl font-bold">MySite</Link>
-                {/*Navigation Links*/}
-                <div className="flex gap-6">
-                    <Link href="/" className="hover:text-gray-300">Home</Link>
-                    <Link href="/about" className="hover:text-gray-300">About</Link>
-                    <Link href="/services" className="hover:text-gray-300">Services</Link>
-                    <Link href="/contact" className="hover:text-gray-300">Contact</Link>
-                </div>
-            </div>
-        </nav>
-    );
+  return(
+  <nav className="bg-gray-900 text-white">
+   <div className="max-w-6xl mx-auto px-4 py-4">
+    <div className="flex justify-between items-center">
+      <Link href ="/" className="text-4xl font-bold mb-4">MySite</Link>
+      <div className="flex gap-6">
+        <Link href="/" className="text-gray-200 hover:text-gray-600">Home</Link>
+        <Link href="/about" className="text-gray-200 hover:text-gray-600">About</Link>
+        <Link href="/contact" className="text-gray-200 hover:text-gray-600">Contact</Link>
+        <Link href="/services" className="text-gray-200 hover:text-gray-600">Services</Link>
+        <Link href="/blog" className="text-gray-200 hover:text-gray-600">Blog</Link>
+        <Link href="/products" className="text-gray-200 hover:text-gray-600">products</Link>
+      </div>
+    </div>
+   </div>
+  </nav>
+  );
 }
+
