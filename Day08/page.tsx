7411597b6a8cc0935fe7bed60c53fb20c8400496
MahrooshNaeemAnwar <mahrooshnaeem10@gmail.com>
2026-09-22 19:46:11 +0500

    added Day08-page.tsx

---
+++ Day08/page.tsx
@@ -0,0 +1,11 @@
+export default function Home(){
+  return (
+    <div className="flex justify-center items-center bg-gray-100 min-h-screen">
+      <div className="bg-blue-500 text-white p-8 rounded-lg">
+        <h1 className="text-3xl font-bold ">Centered!</h1>
+        <p className="text-xl font-thin">flexbox se center kia. </p>
+      </div>
+    </div>
+  );
+}
+    
